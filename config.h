28956043c56258fe00cc8da83ad6dbcf7c57a832
Zy McLeod <zyzy@tuta.io>
2023-11-11 12:11:36 +1100

    Modified config

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=10" };
 static const char dmenufont[]       = "terminus:size=10";
 static const char col_gray1[]       = "#282828";
